8df231f85be15dc74512edeb2b15bd8785c8d510
Monika <codingbymon@gmail.com>
2024-03-25 16:37:51 +0100

    create first variable and style in js

---
+++ script.js
@@ -0,0 +1,22 @@
+const canvas = document.querySelector('#draw')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const ctx = canvas.getContext('2d')
+
+ctx.strokeStyle = "#BADA55"
+ctx.lineJoin = "round"
+ctx.lineCap = "round"
+
+// powyższe style ustawiają kolor rysowania oraz style linii
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// ustawiamy flagę, która ustawia nam opcję rysowania, tylko jeśli przycisk myszy jest wciśnięty, pozwala nam to na przesunięcie kusora bez ingerencji w rysunek
+
+
+
+
