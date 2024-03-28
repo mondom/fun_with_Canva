19c3b0be0799792e85a873cfed3b88451d04406e
Monika <codingbymon@gmail.com>
2024-03-28 16:07:16 +0100

    update draw function

--- script.js
+++ script.js
@@ -16,8 +16,19 @@ let lastX = 0
 let lastY = 0
 
 function draw(e) {
-	console.log(e)
+	if (!isDrawing) return
+	ctx.beginPath()
+    // start from
+	ctx.moveTo(lastX, lastY)
+    // go to
+	ctx.lineTo(e.offsetX, e.offsetY)
+	ctx.stroke()
+    lastX = e.offsetX
+    lastY = e.offsetY
 }
 canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mousedown', () => (isDrawing = true))
+canvas.addEventListener('mouseup', () => (isDrawing = false))
+canvas.addEventListener('mouseout', () => (isDrawing = false))
 
 // ustawiamy flagę, która ustawia nam opcję rysowania, tylko jeśli przycisk myszy jest wciśnięty, pozwala nam to na przesunięcie kusora bez ingerencji w rysunek
