e8c2d5463b3dd189ec9632d8ddd1c96cd46a89a0
Monika <codingbymon@gmail.com>
2024-03-28 14:54:49 +0100

    check function draw

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ canvas.height = window.innerHeight
 
 const ctx = canvas.getContext('2d')
 
-ctx.strokeStyle = "#BADA55"
-ctx.lineJoin = "round"
-ctx.lineCap = "round"
+ctx.strokeStyle = '#BADA55'
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
 
 // powyższe style ustawiają kolor rysowania oraz style linii
 
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+
+function draw(e) {
+	console.log(e)
+}
+canvas.addEventListener('mousemove', draw)
 
 // ustawiamy flagę, która ustawia nam opcję rysowania, tylko jeśli przycisk myszy jest wciśnięty, pozwala nam to na przesunięcie kusora bez ingerencji w rysunek
-
-
-
-
